cd7122a1fa1ba4dc9756db0fa47c623b5b009776
vlnko <laslolnko@gmail.com>
2022-11-25 17:49:43 +0300

    refactored views

--- clientapp/views.py
+++ clientapp/views.py
@@ -3,52 +3,42 @@ from .models import Call
 from .forms import CallForm, LoginForm
 from django.views.generic import DetailView
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
+@login_required(login_url='login')
 def index(request):
-    user = request.user
-
-    if user.is_authenticated:
-        calls = Call.objects.filter(author__company=request.user.company).filter(is_archived=False)
-        return render(request, 'clientapp/index.html', {'title': 'Главная страница', 'calls': calls, 'user': user})
-    else:
-        return redirect('login')
+    calls = Call.objects.filter(author__company=request.user.company).filter(is_archived=False)
+    return render(request, 'clientapp/index.html', {'title': 'Главная страница', 'calls': calls})
 
 
+@login_required(login_url='login')
 def callsarchive(request):
-    user = request.user
-
-    if user.is_authenticated:
-        calls = Call.objects.order_by('-id').filter(author__company=request.user.company).filter(is_archived=True)
-        return render(request, 'clientapp/archive.html', {'title': 'Архив обращений', 'calls': calls})
-    else:
-            return redirect('login')
+    calls = Call.objects.order_by('-id').filter(author__company=request.user.company).filter(is_archived=True)
+    return render(request, 'clientapp/archive.html', {'title': 'Архив обращений', 'calls': calls})
 
 
+@login_required(login_url='login')
 def addnewcall(request):
-    user = request.user
-    
-    if user.is_authenticated:
-        error = ''
-        if request.method == 'POST':
-            form = CallForm(request.POST)
-            if form.is_valid():
-                form = form.save(commit=False)
-                form.author = request.user
-                form.save()
-                return redirect('home')
-            else:
-                error = 'Форма была неверной. Попробуйте еще раз.'
+    error = ''
+    if request.method == 'POST':
+        form = CallForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.author = request.user
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной. Попробуйте еще раз.'
 
-        form = CallForm()
-        context = {
-            'form': form,
-            'error': error,
-        }
+    form = CallForm()
+    context = {
+        'form': form,
+        'error': error,
+    }
 
-        return render(request, 'clientapp/addnewcall.html', context)
-    else:
-        return redirect('login')
+    return render(request, 'clientapp/addnewcall.html', context)
 
 
 def LoginView(request):
@@ -73,9 +63,8 @@ def LogoutView(request):
     return redirect('login')
 
 
-class CallDetailView(DetailView):
+class CallDetailView(LoginRequiredMixin, DetailView):
+    login_url = 'login'
     model = Call
     template_name = 'clientapp/call.html'
     context_object_name = 'call'
-
-
