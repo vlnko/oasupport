af8caa6ed7cb0ffc0881774b896bc49c877b932f
vlnko <laslolnko@gmail.com>
2022-11-25 18:00:18 +0300

    fixed an error in settings.py

--- oasupportapp/settings.py
+++ oasupportapp/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static', 'static_dirs'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
